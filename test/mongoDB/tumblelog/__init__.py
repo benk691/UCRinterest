20da659f69ea52704c7696a4f999a6f0c38dd936
Benjamin Kellogg <benk691@gmail.com>
2013-02-01 08:53:52 -0800

    Updated init file. This commit is a working version. Revert to add more files

--- test/mongoDB/tumblelog/__init__.py
+++ test/mongoDB/tumblelog/__init__.py
@@ -7,5 +7,12 @@ app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
 
+def register_blueprints(app):
+    # Prevents circular imports
+    from tumblelog.views import posts
+    app.register_blueprint(posts)
+
+register_blueprints(app)
+
 if __name__ == '__main__':
 	app.run()
