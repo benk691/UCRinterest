51783dd94d090f0c7390b987d01a9741d27d9100
Benjamin Kellogg <benk691@gmail.com>
2013-02-12 23:59:52 -0800

    Updated to remove non-working changes and the flask virtual env

--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask.ext.wtf import Form, TextField
-from flask.ext.wtf import Required
-
-class LoginForm(Form):
-	name = TextField('name', validators = [Required()])
-	password = TextField('password')
