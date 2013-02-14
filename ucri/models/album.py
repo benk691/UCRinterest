1706877a4c6798c444e9d21e4ce1b1cf9bfc915f
Benjamin Kellogg <benk691@gmail.com>
2013-02-14 02:13:18 -0800

    Updated so that the title is now a link to the home page

--- ucri/models/album.py
+++ ucri/models/album.py
@@ -3,9 +3,16 @@
 # This defines the required document as well as easy-to-use functions to get user information
 ##################################################
 from ucri import db
+from settings import *
 
 class Album(db.Document):
-    creator = db.StringField(min_length = 3, max_length = 25, required = true)
-    title = db.StringField(min_length = 3, max_length = 25, required = true)
-    dscrp = db.StringField(min_length = 3, max_length = 400)
+    '''Album object:
+    creator - creator of album 
+    title - title of album
+    dscrp - description of album
+    pins - pins attached to album
+    '''
+    creator = db.StringField(min_length = NAME_MIN_LENGTH, max_length = NAME_MAX_LENGTH, required = True)
+    title = db.StringField(min_length = NAME_MIN_LENGTH, max_length = NAME_MAX_LENGTH, required = True)
+    dscrp = db.StringField(min_length = DSCRPT_MIN_LENGTH, max_length = DSCRPT_MAX_LENGTH)
     pins = db.ListField()
