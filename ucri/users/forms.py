ab1f77495b2f62f8b4dfe11bc6805cf3adf348b8
Benjamin Kellogg <benk691@gmail.com>
2013-03-04 09:21:58 -0800

    Updated so flash messages stand out and attempt on edit forms, but it does not work

--- ucri/users/forms.py
+++ ucri/users/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form, TextField, TextAreaField, PasswordField, SelectField, TextAreaField, SubmitField, DateTimeField, DateField
 from flask.ext.wtf import Required, Email, EqualTo, Length
-from flask.ext.login import current_user
+from flask.ext.login import current_user, login_required
 from datetime import datetime
 from ucri.models.settings import *
 
@@ -32,24 +32,41 @@ class InterestForm(Form):
     '''
     interest = TextField(u'Interest', [Length(min=INTEREST_MIN_LENGTH, max=INTEREST_MAX_LENGTH)])
 
-@login_required
 class EditForm(Form):
     '''
     Edit form for editing profiles
     '''
-    fname = TextField(u'First Name', [Length(min=NAME_MIN_LENGTH, max=NAME_MAX_LENGTH)], default=current_user.fname)
-    lname = TextField(u'Last Name', [Length(min=NAME_MIN_LENGTH, max=NAME_MAX_LENGTH)], default=current_user.lname)
-    email = TextField(u'Email address', [Email(), Length(min=EMAIL_MIN_LENGTH, max=EMAIL_MAX_LENGTH)], default=current_user.email)
+    # TODO: 
+    #   - Think about entering your old password
+    #   - If checking the current user here doesn't work then do it in the profile edit function
+    def_fname = "Anonymous"
+    def_lname = "NA"
+    def_email = "anon@aol.com"
+    def_gender = 2
+    def_birthday = datetime.now()
+    def_dscrp = "Description"
+
+    if current_user != None:
+        def_fname = current_user.fname
+        def_lname = current_user.lname
+        def_email = current_user.email
+        def_gender = choices=['M', 'F', 'U'].index(current_user.gender)
+        def_birthday = current_user.birthday
+        def_dscrp = current_user.dscrp
+
+    fname = TextField(u'First Name', [Length(min=NAME_MIN_LENGTH, max=NAME_MAX_LENGTH)], default=def_fname)
+    lname = TextField(u'Last Name', [Length(min=NAME_MIN_LENGTH, max=NAME_MAX_LENGTH)], default=def_lname)
+    email = TextField(u'Email address', [Email(), Length(min=EMAIL_MIN_LENGTH, max=EMAIL_MAX_LENGTH)], default=def_email)
     gender = SelectField(
         u'Gender',
         choices=[('M', 'Male'),
                  ('F', 'Female'),
                  ('U', 'Unspecified')],
-        default=current_user.gender)
+        default=def_gender)
     pwd = PasswordField(u'Password',
         [ EqualTo('confirm', message='Passwords must match'), Length(min=PWD_MIN_LENGTH, max=PWD_MAX_LENGTH) ])
     confirm = PasswordField(u'Repeat Password')
     # Birthday
-    bday = DateField(u'Birthday (mm/dd/yyyy)', format='%m/%d/%Y', default=current_user.birthday)
-    dscrp = TextAreaField(u'Describe yourself', [Length(min=DSCRPT_MIN_LENGTH, max=DSCRPT_MAX_LENGTH)], default=current_user.dscrp)
+    bday = DateField(u'Birthday (mm/dd/yyyy)', format='%m/%d/%Y', default=def_birthday)
+    dscrp = TextAreaField(u'Describe yourself', [Length(min=DSCRPT_MIN_LENGTH, max=DSCRPT_MAX_LENGTH)], default=def_dscrp)
     update = SubmitField(u'Update')
